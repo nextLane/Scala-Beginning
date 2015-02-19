61ff46dbc3ff9ebe0668e43953587fa71bf66a6c
nextLane <201201164@daiict.ac.in>
2015-02-19 16:58:24 +0530

    Nested if else

---
+++ sc/NestedIfElse.scala
@@ -0,0 +1,22 @@
+object NestedIfElse{
+
+	def main(args: Array[String]){
+	var x=15;
+
+	if(x<20)
+	{
+		if(x>10)
+			System.out.println("More than 10 but less than 20!")
+		else
+			System.out.println("Less than or equal to 10")
+
+
+	}
+
+	else
+		System.out.println("x is more than 20")
+
+}
+
+
+}
