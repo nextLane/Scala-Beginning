0d367cb6e853e074e028c0ee8841f3c7fbf9ad31
nextLane <201201164@daiict.ac.in>
2015-02-21 16:09:48 +0530

    Func as argument to func| Higher Order Functions

---
+++ sc/HigherFunc.scala
@@ -0,0 +1,17 @@
+object HigherFunc{
+
+	def main(args:Array[String]){
+
+
+		println(apply(layout,10))	
+
+	}
+	
+	def apply(f:Int => String , v:Int)=f(v)
+	def layout[A](x:A) = "["+x.toString()+"]"
+
+
+
+
+
+}
